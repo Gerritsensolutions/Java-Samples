82578bed41dbe048b15588ca5dd24382c3ba0ef1
Gerritsensolutions <45983573+Gerritsensolutions@users.noreply.github.com>
2018-12-18 20:09:19 -0800

    Update Driver.java

--- Multilevel Feedback Queue/Driver.java
+++ Multilevel Feedback Queue/Driver.java
@@ -5,13 +5,11 @@ import java.io.IOException;
  * @author Tyler Gerritsen
  * @version 4-7-2017
  * 
- * Driver class for the MFQ. This is essentially your own driver, professor,
- * with the exception that I switched out the PrintWriter object you used
- * with the SuperOutput object of your own design, for convenience purposes.
+ * Driver class for the MFQ.
  */
 public class Driver {
 	public static void main(String[] args) throws IOException {
-		SuperOutput out = new SuperOutput("csis.txt");
+		SuperOutput out = new SuperOutput("out.txt");
 		MFQ mfq = new MFQ(out);
 		mfq.getJobs();
 		mfq.outputHeader();
